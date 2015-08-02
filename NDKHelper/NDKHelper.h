ad9f4d41ce924f069c33592fb70b2fab931516e8
shauket <mshauket.developer@gmail.com>
2015-08-02 11:12:47 +0400

    Update NDKHelper.h
    update this file after go through

--- NDKHelper/NDKHelper.h
+++ NDKHelper/NDKHelper.h
@@ -3,36 +3,41 @@
 //  EasyNDK-for-cocos2dx
 //
 //  Created by Amir Ali Jiwani on 23/02/2013.
-//  Updated by Alfonso Cejudo March, 2014.
-//
+// updated by muhammad shauket on 1/08/2015
 //
 
 #ifndef __EasyNDK_for_cocos2dx__NDKHelper__
 #define __EasyNDK_for_cocos2dx__NDKHelper__
 
+#include <iostream>
 #include "cocos2d.h"
+#include <string>
+#include <vector>
 #include "jansson.h"
 #include "NDKCallbackNode.h"
+USING_NS_CC;
+using namespace std;
 
 class NDKHelper
 {
-public:
-    static void addSelector(const char *groupName, const char *name, FuncNV selector, cocos2d::Node *target);
-    static void printSelectorList();
-    static void removeSelectorsInGroup(const char *groupName);
-    
-    static cocos2d::Value getValueFromJson(json_t *obj);
-    static json_t *getJsonFromValue(cocos2d::Value value);
-    
-    static void handleMessage(json_t *methodName, json_t *methodParams);
-    
-private:
-    static std::vector<NDKCallbackNode> selectorList;
+    private :
+        static std::vector<NDKCallbackNode> selectorList;
+        //static CCDictionary* GetDict(json_t *dictionary);
+        static void RemoveAtIndex(int index);
+        static const char* TargetGroupName;
+
+    public :
+        static void AddSelector(const char *groupName, const char *name, SEL_CallFuncND selector, Node* target);
+        static void RemoveSelectorsInGroup();
+        static void PrintSelectorList();
+        static Ref* GetCCObjectFromJson(json_t *obj);
+        static json_t* GetJsonFromCCObject(Ref* obj);
+        static void HandleMessage(json_t *methodName, json_t* methodParams);
 };
 
 extern "C"
 {
-    void sendMessageWithParams(std::string methodName, cocos2d::Value methodParams);
+    void SendMessageWithParams(string methodName, Ref* methodParams);
 }
 
 #endif /* defined(__EasyNDK_for_cocos2dx__NDKHelper__) */

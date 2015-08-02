61c2b73053bfda3528dad87e55fc839b71823a1f
shauket <mshauket.developer@gmail.com>
2015-08-02 11:11:14 +0400

    Update NDKHelper.cpp
    this is an updated file kindly go through it.

--- NDKHelper/NDKHelper.cpp
+++ NDKHelper/NDKHelper.cpp
@@ -3,180 +3,223 @@
 //  EasyNDK-for-cocos2dx
 //
 //  Created by Amir Ali Jiwani on 23/02/2013.
-//  Updated by Alfonso Cejudo March, 2014.
 //
 //
 
 #include "NDKHelper.h"
-#include "CallFuncNV.h"
-
-USING_NS_CC;
 
 #define __CALLED_METHOD__           "calling_method_name"
 #define __CALLED_METHOD_PARAMS__    "calling_method_params"
 
 std::vector<NDKCallbackNode> NDKHelper::selectorList;
-
-void NDKHelper::addSelector(const char *groupName, const char *name, FuncNV selector, Node *target)
+const char* NDKHelper::TargetGroupName = nullptr;
+void NDKHelper::AddSelector(const char *groupName, const char *name, SEL_CallFuncND selector, Node* target)
 {
     NDKHelper::selectorList.push_back(NDKCallbackNode(groupName, name, selector, target));
+    NDKHelper::TargetGroupName = groupName;
 }
 
-void NDKHelper::printSelectorList()
+void NDKHelper::RemoveAtIndex(int index)
 {
-    for (unsigned int i = 0; i < NDKHelper::selectorList.size(); ++i) {
-        std::string s = NDKHelper::selectorList[i].getGroup();
-        s.append(NDKHelper::selectorList[i].getName());
-    }
+    NDKHelper::selectorList[index] = NDKHelper::selectorList.back();
+    NDKHelper::selectorList.pop_back();
 }
 
-void NDKHelper::removeSelectorsInGroup(const char *groupName)
+void NDKHelper::RemoveSelectorsInGroup()
 {
     std::vector<int> markedIndices;
-    
-    for (unsigned int i = 0; i < NDKHelper::selectorList.size(); ++i) {
-        if (NDKHelper::selectorList[i].getGroup().compare(groupName) == 0) {
-            markedIndices.push_back(i);
-        }
-    }
-    
-    for (long i = markedIndices.size() - 1; i >= 0; --i) {
-        NDKHelper::selectorList.erase(NDKHelper::selectorList.begin() + markedIndices[i]);
-    }
-}
+    CCLOG("vectore size %lu",NDKHelper::selectorList.size());
 
-Value NDKHelper::getValueFromJson(json_t *obj)
-{
-    if (obj == NULL) {
-        return Value::Null;
+    for (auto& keyval : NDKHelper::selectorList)
+    {
+        string groupname = keyval.getGroup();
+        std::string targeTname = NDKHelper::TargetGroupName;
+        
+
+//        if (!groupname.compare(targeTname))
+//        {
+            NDKHelper::selectorList.pop_back();
+//        }
     }
     
-    if (json_is_object(obj)) {
-        ValueMap valueMap;
+  }
+
+Ref* NDKHelper::GetCCObjectFromJson(json_t *obj)
+{
+    if (obj == NULL)
+        return NULL;
+    
+    if (json_is_object(obj))
+    {
+        CCDictionary *dictionary = new CCDictionary();
+        //CCDictionary::create();
         
         const char *key;
         json_t *value;
         
         void *iter = json_object_iter(obj);
-        while (iter) {
+        while(iter)
+        {
             key = json_object_iter_key(iter);
             value = json_object_iter_value(iter);
             
-            valueMap[key] = NDKHelper::getValueFromJson(value);
+            dictionary->setObject(NDKHelper::GetCCObjectFromJson(value)->autorelease(), string(key));
             
             iter = json_object_iter_next(obj, iter);
         }
         
-        return Value(valueMap);
-    } else if (json_is_array(obj)) {
-        ValueVector valueVector;
-        
-        size_t sizeArray = json_array_size(obj);
-        
-        for (unsigned int i = 0; i < sizeArray; i++) {
-            valueVector.push_back(NDKHelper::getValueFromJson(json_array_get(obj, i)));
-        }
-        
-        return Value(valueVector);
-    } else if (json_is_boolean(obj)) {
-        if (json_is_true(obj)) {
-            return Value(true);
-        } else {
-            return Value(false);
-        }
-    } else if (json_is_integer(obj)) {
-        int value = (int) json_integer_value(obj);
-        
-        return Value(value);
-    } else if (json_is_real(obj)) {
-        double value = json_real_value(obj);
-        
-        return Value(value);
-    } else if (json_is_string(obj)) {
-        std::string value = json_string_value(obj);
-        
-        return Value(value);
+        return dictionary;
     }
-    
-    return Value::Null;
-}
-
-json_t *NDKHelper::getJsonFromValue(Value value)
-{
-    if (value.getType() == Value::Type::MAP) {
-        ValueMap valueMap = value.asValueMap();
+    else if (json_is_array(obj))
+    {
+        size_t sizeArray = json_array_size(obj);
+        CCArray *array = new CCArray();
+        //CCArray::createWithCapacity(sizeArray);
         
-        json_t *jsonDict = json_object();
-        
-        for (auto &element : valueMap) {
-            json_object_set_new(jsonDict, element.first.c_str(),
-                                NDKHelper::getJsonFromValue(element.second));
+        for (unsigned int i = 0; i < sizeArray; i++)
+        {
+            array->addObject(NDKHelper::GetCCObjectFromJson(json_array_get(obj, i))->autorelease());
         }
         
-        return jsonDict;
-    } else if (value.getType() == Value::Type::VECTOR) {
-        ValueVector valueVector = value.asValueVector();
+        return array;
+    }
+    else if (json_is_boolean(obj))
+    {
+        stringstream str;
+        if (json_is_true(obj))
+            str << true;
+        else if (json_is_false(obj))
+            str << false;
         
-        json_t *jsonArray = json_array();
+        CCString *ccString = new CCString(str.str());
+        //CCString::create(str.str());
+        return ccString;
+    }
+    else if (json_is_integer(obj))
+    {
+        stringstream str;
+        str << json_integer_value(obj);
         
-        size_t sizeVector = valueVector.size();
+        CCString *ccString = new CCString(str.str());
+        //CCString::create(str.str());
+        return ccString;
+    }
+    else if (json_is_real(obj))
+    {
+        stringstream str;
+        str << json_real_value(obj);
         
-        for (unsigned int i = 0; i < sizeVector; i++) {
-            json_array_append_new(jsonArray,
-                                  NDKHelper::getJsonFromValue(valueVector.at(i)));
-        }
+        CCString *ccString = new CCString(str.str());
+        //CCString::create(str.str());
+        return ccString;
+    }
+    else if (json_is_string(obj))
+    {
+        stringstream str;
+        str << json_string_value(obj);
         
-        return jsonArray;
-    } else if (value.getType() == Value::Type::BOOLEAN) {
-        return json_boolean(value.asBool());
-    } else if (value.getType() == Value::Type::INTEGER) {
-        return json_integer(value.asInt());
-    } else if (value.getType() == Value::Type::DOUBLE) {
-        return json_real(value.asDouble());
-    } else if (value.getType() == Value::Type::STRING) {
-        return json_string(value.asString().c_str());
+        CCString *ccString = new CCString(str.str());
+        //CCString::create(str.str());
+        return ccString;
     }
     
     return NULL;
 }
 
-void NDKHelper::handleMessage(json_t *methodName, json_t *methodParams)
+json_t* NDKHelper::GetJsonFromCCObject(Ref* obj)
 {
-    if (methodName == NULL) {
-        return;
+    if (dynamic_cast<CCDictionary*>(obj))
+    {
+        CCDictionary *mainDict = (CCDictionary*)obj;
+        CCArray *allKeys = mainDict->allKeys();
+        json_t* jsonDict = json_object();
+        
+        if(allKeys == NULL ) return jsonDict;
+        for (unsigned int i = 0; i < allKeys->count(); i++)
+        {
+            const char *key = ((CCString*)allKeys->objectAtIndex(i))->getCString();
+            json_object_set_new(jsonDict,
+                                key,
+                                NDKHelper::GetJsonFromCCObject(mainDict->objectForKey(key)));
+        }
+        
+        return jsonDict;
     }
+    else if (dynamic_cast<CCArray*>(obj))
+    {
+        CCArray* mainArray = (CCArray*)obj;
+        json_t* jsonArray = json_array();
+        
+        for (unsigned int i = 0; i < mainArray->count(); i++)
+        {
+            json_array_append_new(jsonArray,
+                                  NDKHelper::GetJsonFromCCObject(mainArray->objectAtIndex(i)));
+        }
+        
+        return jsonArray;
+    }
+    else if (dynamic_cast<CCString*>(obj))
+    {
+        CCString* mainString = (CCString*)obj;
+        json_t* jsonString = json_string(mainString->getCString());
+        
+        return jsonString;
+    }
+    
+    return NULL;
+}
+
+void NDKHelper::PrintSelectorList()
+{
+    for (unsigned int i = 0; i < NDKHelper::selectorList.size(); ++i)
+    {
+        string s = NDKHelper::selectorList[i].getGroup();
+        s.append(NDKHelper::selectorList[i].getName());
+        CCLog(s.c_str());
+    }
+}
+
+void NDKHelper::HandleMessage(json_t *methodName, json_t* methodParams)
+{
+    if (methodName == NULL)
+        return;
     
     const char *methodNameStr = json_string_value(methodName);
     
-    for (unsigned int i = 0; i < NDKHelper::selectorList.size(); ++i) {
-        if (NDKHelper::selectorList[i].getName().compare(methodNameStr) == 0) {
-            Value value = NDKHelper::getValueFromJson(methodParams);
+    for (unsigned int i = 0; i < NDKHelper::selectorList.size(); ++i)
+    {
+        string groupname = NDKHelper::selectorList[i].getGroup();
+        std::string targeTname = NDKHelper::TargetGroupName;
+        
+     //   const char* groupName = NDKHelper::selectorList[i].getGroup().c_str();
+        
+        if (NDKHelper::selectorList[i].getName().compare(methodNameStr) == 0 && !groupname.compare(targeTname))
+        {
+            CCObject *dataToPass = NDKHelper::GetCCObjectFromJson(methodParams);
             
-            FuncNV sel = NDKHelper::selectorList[i].getSelector();
+            if (dataToPass != NULL)
+                dataToPass->retain();
+            
+            SEL_CallFuncND sel = NDKHelper::selectorList[i].getSelector();
             Node *target = NDKHelper::selectorList[i].getTarget();
             
-            CallFuncNV *caller = CallFuncNV::create(sel);
-            caller->setValue(value);
+            CCFiniteTimeAction* action = CCSequence::create(CCCallFuncND::create(target, sel, (void*)dataToPass), NULL);
             
-            if (target) {
-                FiniteTimeAction *action = Sequence::create(caller, NULL);
-                
-                target->runAction(action);
-            } else {
-                caller->execute();
-            }
+            target->runAction(action);
             
+            if (dataToPass != NULL)
+                dataToPass->autorelease();
             break;
         }
     }
 }
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-#include "../../cocos2d/cocos/2d/platform/android/jni/JniHelper.h"
+#include "../cocos2dx/platform/android/jni/JniHelper.h"
 #include <android/log.h>
 #include <jni.h>
+#define  LOG_TAG    "EasyNDK-for-cocos2dx"
 
-#define LOG_TAG    "EasyNDK-for-cocos2dx"
 #define CLASS_NAME "com/easyndk/classes/AndroidNDKHelper"
 #endif
 
@@ -187,33 +230,18 @@ void NDKHelper::handleMessage(json_t *methodName, json_t *methodParams)
 extern "C"
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-    // Method for receiving NDK messages from Java, Android
-    void Java_com_easyndk_classes_AndroidNDKHelper_CPPNativeCallHandler(JNIEnv *env, jobject thiz, jstring json) {
-        /* The JniHelper call resulted in crash, so copy the jstring2string method here */
-        //std::string jsonString = JniHelper::jstring2string(json);
-        if (json == NULL) {
-            return;
-        }
-        
-        JNIEnv *pEnv = JniHelper::getEnv();
-        if (!env) {
-            return;
-        }
-        
-        const char *chars = env->GetStringUTFChars(json, NULL);
-        std::string ret(chars);
-        env->ReleaseStringUTFChars(json, chars);
-        
-        std::string jsonString = ret;
-        /* End jstring2string code */
-        
+    // Method for recieving NDK messages from Java, Android
+    void Java_com_easyndk_classes_AndroidNDKHelper_CPPNativeCallHandler(JNIEnv* env, jobject thiz, jstring json)
+    {
+        string jsonString = JniHelper::jstring2string(json);
         const char *jsonCharArray = jsonString.c_str();
         
         json_error_t error;
         json_t *root;
         root = json_loads(jsonCharArray, 0, &error);
         
-        if (!root) {
+        if (!root)
+        {
             fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
             return;
         }
@@ -223,57 +251,59 @@ extern "C"
         jsonMethodParams = json_object_get(root, __CALLED_METHOD_PARAMS__);
         
         // Just to see on the log screen if messages are propogating properly
-        // __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, jsonCharArray);
+        __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, jsonCharArray);
         
-        NDKHelper::handleMessage(jsonMethodName, jsonMethodParams);
+        NDKHelper::HandleMessage(jsonMethodName, jsonMethodParams);
         json_decref(root);
     }
 #endif
     
-    // Method for sending message from CPP to the targeted platform
-    void sendMessageWithParams(std::string methodName, Value methodParams) {
-        if (0 == strcmp(methodName.c_str(), "")) {
+    // Method for sending message from CPP to the targetted platform
+    void SendMessageWithParams(string methodName, CCObject* methodParams)
+    {
+        if (0 == strcmp(methodName.c_str(), ""))
             return;
-        }
         
         json_t *toBeSentJson = json_object();
         json_object_set_new(toBeSentJson, __CALLED_METHOD__, json_string(methodName.c_str()));
         
-        if (!methodParams.isNull()) {
-            json_t *paramsJson = NDKHelper::getJsonFromValue(methodParams);
-            
+        if (methodParams != NULL)
+        {
+            json_t* paramsJson = NDKHelper::GetJsonFromCCObject(methodParams);
             json_object_set_new(toBeSentJson, __CALLED_METHOD_PARAMS__, paramsJson);
         }
         
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
         JniMethodInfo t;
         
-		if (JniHelper::getStaticMethodInfo(t,
+        if (JniHelper::getStaticMethodInfo(t,
                                            CLASS_NAME,
-                                           "ReceiveCppMessage",
-                                           "(Ljava/lang/String;)V")) {
-            char *jsonStrLocal = json_dumps(toBeSentJson, JSON_COMPACT | JSON_ENSURE_ASCII);
-            std::string jsonStr(jsonStrLocal);
+                                           "RecieveCppMessage",
+                                           "(Ljava/lang/String;)V"))
+        {
+            char* jsonStrLocal = json_dumps(toBeSentJson, JSON_COMPACT | JSON_ENSURE_ASCII);
+            string jsonStr(jsonStrLocal);
             free(jsonStrLocal);
             
             jstring stringArg1 = t.env->NewStringUTF(jsonStr.c_str());
             t.env->CallStaticVoidMethod(t.classID, t.methodID, stringArg1);
             t.env->DeleteLocalRef(stringArg1);
-			t.env->DeleteLocalRef(t.classID);
-		}
+            t.env->DeleteLocalRef(t.classID);
+        }
 #endif
         
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
         json_t *jsonMessageName = json_string(methodName.c_str());
         
-        if (!methodParams.isNull()) {
-            json_t *jsonParams = NDKHelper::getJsonFromValue(methodParams);
-            
-            IOSNDKHelperImpl::receiveCPPMessage(jsonMessageName, jsonParams);
-            
+        if (methodParams != NULL)
+        {
+            json_t *jsonParams = NDKHelper::GetJsonFromCCObject(methodParams);
+            IOSNDKHelperImpl::RecieveCPPMessage(jsonMessageName, jsonParams);
             json_decref(jsonParams);
-        } else {
-            IOSNDKHelperImpl::receiveCPPMessage(jsonMessageName, NULL);
+        }
+        else
+        {
+            IOSNDKHelperImpl::RecieveCPPMessage(jsonMessageName, NULL);
         }
         
         json_decref(jsonMessageName);
